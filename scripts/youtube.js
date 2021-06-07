c08f9d68af7ad59ff20fd94bb58be87169fba6ac
Help-14 <help-14@hotmail.com>
2021-06-07 21:16:28 +0700

    Update youtube.js

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -5,6 +5,8 @@
 // @description  Go to theater mode automatically
 // @author       Help-14
 // @match        *://*.youtube.com/watch?v=*
+// @updateURL    https://raw.githubusercontent.com/help-14/TamperMonkey-Scripts/main/scripts/youtube.js
+// @downloadURL  https://raw.githubusercontent.com/help-14/TamperMonkey-Scripts/main/scripts/youtube.js
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
 // @grant        none
 // ==/UserScript==
