38183b430516c65b1cbd6a1c060dba705a189017
Help-14 <help-14@hotmail.com>
2021-08-04 18:02:31 +0700

    Update vcomic.js

--- scripts/vcomic.js
+++ scripts/vcomic.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Vcomic
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Hide "please disable adblock" popup
 // @author       Help-14
 // @match        *://vcomic.net/*
+// @match        *://truyen86.com/*
 // @updateURL    https://raw.githubusercontent.com/help-14/TamperMonkey-Scripts/main/scripts/vcomic.js
 // @downloadURL  https://raw.githubusercontent.com/help-14/TamperMonkey-Scripts/main/scripts/vcomic.js
 // @icon         https://www.google.com/s2/favicons?domain=vcomic.net
